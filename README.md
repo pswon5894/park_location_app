a15a46304df9387ce72020385f13b354cafbae6e
pswon5894 <pswon5894@gmail.com>
2025-10-26 19:22:37 +0900

    readme.md - 사용자 시나리오 작성 main.dart, location_screen.dart - 제목만 표시되는 화면 작성 pubspec.yaml - flutter pub add flutter_map (패키지 추가)

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
 # park_location_app
 
-A new Flutter project.
+주차 위치 공유 앱
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+프레임워크   flutter
+언어          dart
+
+flutter_map 패키지 사용, api key 불필요
+
+### 사용 시나리오
+
+켜면 지도가 나오는 화면
+
+위치 버튼을 누르면 현재 위치가 지도에 마크 표시됨
+
+공유 버튼으로 링크를 만들어 보내
+
+구글맵스에서 현재 위치를 볼수 있음
 
 A few resources to get you started if this is your first Flutter project:
 
---
+++ lib/screen/location_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart'
+
+class LocationScreen extends StatefulWidget {
+  const LocationScreen({super.key});
+
+  @override
+  State<LocationScreen> createState() => _LocationScreenState();
+}
+
+class _LocationScreenState extends State<LocationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        title: Text('주차 위치 공유'),
+      ),
+    );
+  }
+}
