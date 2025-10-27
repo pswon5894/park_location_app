e135e0c4e450bc9bf397446a7d2c59ab14518e67
pswon5894 <pswon5894@gmail.com>
2025-10-27 18:09:18 +0900

    location_screen.dart -  공유 버튼과 위치 버튼 코드 기능 구현 AndroidManifest.xml - 위치 접근 설정과 인터넷 접근 설정 앱 구현 끝 켜면 서울로 나오고 gps 안켜져 있으면 켜달라고 하고 현재 위치 버튼 누르면 위치로 이동 공유 버튼시 구글 맵스로 좌표 찍어서 링크로 보낼 수 있음

--- lib/screen/location_screen.dart
+++ lib/screen/location_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:location/location.dart';
+import 'package:share_plus/share_plus.dart';
 
 class LocationScreen extends StatefulWidget {
   const LocationScreen({super.key});
@@ -10,6 +12,44 @@ class LocationScreen extends StatefulWidget {
 }
 
 class _LocationScreenState extends State<LocationScreen> {
+
+  final Location location = Location();
+  final MapController mapController = MapController();
+  LatLng _currentCenter = LatLng(37.5665, 126.9780); // 서울 시청
+  LocationData? _currentLocation;
+
+  Future<void> _shareLocationOnly() async {
+
+    final locationText =
+        '내 위치 \n https://maps.google.com/?q=${_currentCenter.latitude},${_currentCenter.longitude}';
+    await SharePlus.instance.share(
+        ShareParams(
+          text:  locationText,
+        ));
+
+  }
+
+  Future<void> _getCurrentLocation() async {
+    bool serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) return;
+    }
+
+    PermissionStatus permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) return;
+    }
+
+    final loc = await location.getLocation();
+    setState(() {
+      _currentLocation = loc;
+      _currentCenter = LatLng(loc.latitude!, loc.longitude!);
+      mapController.move(_currentCenter, 16.0);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,41 +58,53 @@ class _LocationScreenState extends State<LocationScreen> {
       body: Stack(
           children: [
             FlutterMap(
+              mapController: mapController,
               options: MapOptions(
-                initialCenter: LatLng(37.5665, 126.9780), // 서울 좌표
+                initialCenter: _currentCenter,
                 initialZoom: 9.2,
               ),
               children: [
                 TileLayer(
                   urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                 ),
+                MarkerLayer(markers: _currentLocation != null
+                ? [
+                  Marker(
+                    point: LatLng(
+                      _currentLocation!.latitude!,
+                      _currentLocation!.longitude!,
+                    ),
+                    width: 80,
+                    height: 80,
+                    child: const Icon(
+                      Icons.location_pin,
+                      color: Colors.red,
+                      size: 40,
+                    ),
+                  ),
+                ]
+                : [],
+                )
               ],
             ),
             Positioned(
               bottom: 120,
               right: 20,
               child: FloatingActionButton(
-                onPressed: () {
-                  // 위치 아이콘 버튼을 눌렀을 때 실행될 코드를 여기에 작성합니다.
-                  print('공유 아이콘 버튼 클릭됨');
-                },
-                child: Icon(Icons.share),
+                onPressed: _shareLocationOnly,
                 tooltip: '좌표 링크 공유',
+                child: Icon(Icons.share),
               ),
             ),
             Positioned(
               bottom: 20,
               right: 20,
               child: FloatingActionButton(
-                onPressed: () {
-                  // 위치 아이콘 버튼을 눌렀을 때 실행될 코드를 여기에 작성합니다.
-                  print('위치 아이콘 버튼 클릭됨');
-                },
-                child: Icon(Icons.my_location),
+                onPressed: _getCurrentLocation,
                 tooltip: '현재 위치로 이동',
+                child: Icon(Icons.my_location),
               ),
             ),
-
           ]
       ),
     );
